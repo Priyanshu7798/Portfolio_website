b4f03aca310fd751169b66404fe42d992ca5d10d
Priyanshu7798 <124646393+Priyanshu7798@users.noreply.github.com>
2024-05-28 19:45:23 +0530

    done untill footer

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+var img=document.getElementById('oneImg').addEventListener('mouseover',()=>{
+    document.getElementById('oneImg').src="./assets/project-2.png"
+})
+
+var img=document.getElementById('oneImg').addEventListener('mouseleave',()=>{
+    document.getElementById('oneImg').src="./assets/project-1.png"
+})
